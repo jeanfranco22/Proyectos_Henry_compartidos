cc1e0d7e51e7da46bda0e69c231a2fab022ba5ad
jean <jeanferrermendez@MacBook-Air-de-jean.local>
2025-04-10 12:04:11 -0600

    cambios desde macbook

---
+++ modulo1/index.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Jean Franco Ferrer Mendez</h1>
+        <h2>Mi Pagina Web</h2>
+        <nav>
+            <ul>
+                <li>¿ Quien Soy ?</li>
+                <li>Mis Tecnologias Favoritas</li>
+                <li>Mis Actividades Favoritas</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h3>Quien Soy</h3>
+        <span>
+            Soy un chico de Puebla, Mexico apasionado por la tecnologia, y el aprender cosas nuevas, tengo 29 años y actualmente soy licenciado en 
+            nutricion clinica y deportiva, me encuentro cursando el modulo 1 del bootcamp en SoyHenry, deseando poder aprender mas y desarrollar mejores y mas habilidades en el
+            ambito de la programacion
+        </span>
+        <h4>Mis Tecnologias Favoritas</h4>
+        <span>
+            <img src="./HTML-5 Logo.png" alt="Esta es una imagen de html" id="logo1">
+            <img src="./css logo.png" alt="Esta es una imagen de CSS" id="logo2">
+            <img src="./JavaScript-logo.png" alt="Esta es una imagen de JS" id="logo3">
+        </span>
+        <h5>Mis Actividades Favoritas</h5>
+        <span>
+            
+        </span>
+        
+        
+
+
+
+
+    </main>
+    
+</body>
+</html>
